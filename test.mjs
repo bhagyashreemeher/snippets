a790a44b46fd9b04f82fa8ad4041edc2000f6b5b
Bhagyashree Meher <42805308+bhagyashreemeher@users.noreply.github.com>
2023-05-25 01:43:40 +0530

    Update test.mjs

--- test.mjs
+++ test.mjs
@@ -951,115 +951,15 @@ expect( rootContainer.querySelector('.delete-batch-item-modal') ).to.equal( null
 
     describe('filter by shipment', () => {
       describe('when checked', async () => {
-        before(async function () {
-            // Create test batch
-            await act(
-              async () =>
-                (this.batch = await app.createBatch({
-                  ship_from: '1',
-                  marketplace: 'US',
-                  name: 'Test Batch Items',
-                }))
-            );
-      
-            // Load inventory info for given MSKUs
-            await act(
-              async () =>
-                (this.inventory = await app.loadInventory({
-                  filters: {
-                    q: [
-                      'B001FOQJOG-076',
-                      '2013-04-19-B004CRYE2C-UA',
-                      '0553537865-PM',
-                      '2019-02-26-B01AXY5YD0-868',
-                    ],
-                  },
-                }))
-            );
-      
-            act(() => app.setState({ isWarehouse: true }));
-      
-            // Create the batch items
-            await act(
-              async () =>
-                (this.batchItems = await app.createBatchItems(
-                  this.batch._id,
-                  this.inventory.data.map((inventory) => ({
-                    inventory,
-                    quantity: 2,
-                    buylist: [{ cost: 500 }],
-                  }))
-                ))
-            );
-      
-            // Load batch items
-            act(() => app.setState({ currentBatch: this.batch._id }));
-            await act(() => app.loadBatchItems(this.batch._id));
-      
-            const mock_inventory = {
-              condition: 'NewItem',
-              fnsku: 'X002L3W3OL',
-              msku: 'B001FOQJOG-076',
-              marketplace: 'US',
-              price: 19998,
-              misc: {},
-              product: {
-                title: 'His Crown is pechalat by Jig',
-                asin: 'B001FOQJOG',
-                image: 'https://m.media-amazon.com/images/I/41i9dZlSsmL.jpg',
-                misc: {
-                  dim: { width: 1499, height: 1349, length: 2474, weight: 2255 },
-                },
-                upc: ['9781477058435'],
-                rank: 123456,
-              },
-            };
-            // fake the update
-            act(() => {
-              this.batchItems.map((item, i) =>
-                app.onBatchItemUpdated({
-                  _id: item._id,
-                  batch_id: item.batch_id,
-                  quantity: i === 0 ? 1 : i === 1 ? 2 : 0,
-                  damaged: i === 0 ? 1 : 0,
-                  shipments: i === 0 ? ['FBA123456789'] : null,
-                  errors: i === 2 ? ['Error!'] : null,
-                  inventory: {
-                    ...mock_inventory,
-                    product_feed_status: i === 0 ? 'HasErrors' : null,
-                    product_feed_errors: i === 0 ? ['Failed!'] : null,
-                    product_feed_batch_id: i === 0 ? this.batch._id : null,
-                    inventory_feed_status: i === 1 ? 'HasErrors' : null,
-                    inventory_feed_errors: i === 1 ? ['Failed!'] : null,
-                    inventory_feed_batch_id: i === 1 ? this.batch._id : null,
-                  },
-                  plans:
-                    i === 0
-                      ? {
-                          plans: [
-                            {
-                              id: 'FBA123456789',
-                              fcid: 'FTW3',
-                              prep: 'SELLER_LABEL',
-                            },
-                          ],
-                        }
-                      : null,
-                })
-              );
-            });
-          });
-
         it('should show list of filters', async () => {
-          console.log(rootContainer.querySelector('.batch-items-filer').innerHTML);
-          console.log(rootContainer.querySelector('.batch-header').innerHTML);
-          
           act(() =>
             app.setState({
               filterItemsToReceive: false,
               multipackCalculateShipmentQuantity: false,
             })
           );
+          await until(() => !app.state.batches.loading);
+
           await until(() => !app.state.batches.loading);
 
           const bisDom = rootContainer.querySelectorAll(
@@ -1095,7 +995,6 @@ expect( rootContainer.querySelector('.delete-batch-item-modal') ).to.equal( null
               .textContent
           ).to.match(/1 SKU/);
         });
-
       });
     });
   });
